77b523b90bc421d6831dba7da1191ce30c2a998e
Luyo Samson <luyosamson@gmail.com>
2024-06-18 23:51:33 +0300

    model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+Class Post(models.Model):
+    title=models.CharField(max_length=250)
+    slud=models.SlugField(max_length=250)
+    body=models.TextField()
+
+    def __str__(self):
+        return self.title
