5ff2787323c6b133b0b0695fe4a9dc6f329e887f
facelessDev <luyosamson@gmail.com>
2024-07-09 00:51:26 +0300

    Modified

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
@@ -11,11 +12,14 @@ class Post(models.Model):
 
     title=models.CharField(max_length=250)
     slug=models.SlugField(max_length=250)
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
     body=models.TextField()
     publish=models.DateTimeField(default=timezone.now)
     created=models.DateTimeField(auto_now_add=True)
     updated=models.DateTimeField(auto_now=True)
-    status=models.CharField(max_length=2,choices=Status.choices,draft=Status.DRAFT)
+    status=models.CharField(max_length=2,
+                            choices=Status.choices,
+                            default=Status.DRAFT)
 
 
     class Meta:
